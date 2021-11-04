90325540c44105500e640ea3830439f76f9eb07b
Alex Pastel <alex.r.pastel@gmail.com>
2021-11-03 22:44:01 -0600

    Set login time on linux

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -5,16 +5,19 @@ from ui_form import Ui_MainWindow
 
 import sys
 import platform
-import win32net
-import win32api
-import time
+from datetime import datetime
+import subprocess
 
-def get_logged_in_time():
+def get_login_time():
     os_name = platform.uname()[0].lower()
     if os_name == "windows":
         user = win32net.NetUserGetInfo(None,win32api.GetUserName(),2)
         print(user["last_logon"])
-    elif os_name.endswith("nix"):
+    elif os_name.endswith("linux"):
+        cmd = "last -R $USER -s 00:00 | perl -ne 'print unless /wtmp\sbegins/ || /^$/' | awk 'END {print $6}'"
+        first_login_time = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout.partition('\n')[0]
+        first_login_time = datetime.strptime(first_login_time, '%H:%M')
+        return QTime(first_login_time.hour, first_login_time.minute)
         
 
 if __name__ == "__main__":
@@ -22,7 +25,6 @@ if __name__ == "__main__":
     ex = Ui_MainWindow()
     w = QMainWindow()
     ex.setupUi(w)
-    get_logged_in_time()
-    ex.startTime.setTime(QTime(7, 43))
+    ex.startTime.setTime(get_login_time())
     w.show()
     sys.exit(app.exec_())
